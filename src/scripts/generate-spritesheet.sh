b57423846747a3ab0d293567e1ee9a1fe48c85a8
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2020-08-31 11:57:27 +0200

    improve ref pages

--- src/scripts/generate-spritesheet.sh
+++ src/scripts/generate-spritesheet.sh
@@ -89,7 +89,8 @@ html="<!DOCTYPE html>
     <meta charset=\"utf-8\"/>
     <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"/>
     <meta name=\"theme-color\" content=\"#ffde41\"/>
-    <title>Pokémon Assets - ${_OUT_NAME} sprite sheet</title>
+    <title>Pokémon Assets | ${_OUT_NAME} spritesheet | Pokémon menu icons</title>
+    <link rel=\"shortcut icon\" type=\"image/png\" href=\"https://itsjavi.com/pokemon-assets/assets/img/symbols/ui-search.png\"/>
     <link href=\"https://cdnjs.cloudflare.com/ajax/libs/normalize/4.1.1/normalize.min.css\" rel=\"stylesheet\" type=\"text/css\">
     <link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css?family=Exo+2:300,400,500\">
     <link href=\"css/styles.css\" rel=\"stylesheet\" type=\"text/css\">
@@ -127,6 +128,7 @@ html="<!DOCTYPE html>
         padding:20px;
         font-family: Courier, \"Courier New\", monospace;
       }
+      /*#css-class-viewer::selection { background: #ffde41; color: #000; }*/
 
       #home-btn {
         display: inline-block;
@@ -151,6 +153,7 @@ html="<!DOCTYPE html>
         0 2px 0 #000,
         -2px 2px 0 #000,
         -2px 0 0 #000;
+        text-transform: capitalize;
       }
     </style>
     <script async src=\"https://www.googletagmanager.com/gtag/js?id=UA-85082661-2\"></script>
@@ -164,7 +167,7 @@ html="<!DOCTYPE html>
 <body>
 
 <a id=\"home-btn\" href=\"https://itsjavi.com/pokemon-assets\">
-<img alt=\"logo\" src=\"https://itsjavi.com/pokemon-assets/assets/img/symbols/pokeball-logo.png\" /></a>
+<img alt=\"logo\" src=\"https://itsjavi.com/pokemon-assets/assets/img/symbols/ui-search.png\" /></a>
 <h1 id=\"main-title\">${_OUT_NAME}</h1><br>"
 
 for img in *.png; do
@@ -195,7 +198,7 @@ for img in *.png; do
   @include sprite-rect(${W}px, ${H}px, ${X_POS}px, ${Y_POS}px);
 }"
   html="$html
-    <span title=\".${_CSS_PREFIX} .${_CSS_PREFIX}-${ALIAS}\" class='${_CSS_PREFIX}-wrapper'>
+    <span title=\"${ALIAS}\" class='${_CSS_PREFIX}-wrapper'>
       <i class=\"${_CSS_PREFIX} ${_CSS_PREFIX}-${ALIAS}\"></i>
     </span>
   "
@@ -244,12 +247,28 @@ css="${sprite_wrapper_css} $css"
 scss="${sprite_wrapper_css} $scss"
 
 html="$html
-<div id=\"css-class-viewer\">  - Click on any sprite to see the HTML code here -  </div>
+<div id=\"css-class-viewer\" tabindex=\"0\">  - Hover on any sprite to see and select the HTML code here -  </div>
 <script>
+function selectText(node) {
+    if (document.body.createTextRange) {
+        const range = document.body.createTextRange();
+        range.moveToElementText(node);
+        range.select();
+    } else if (window.getSelection) {
+        const selection = window.getSelection();
+        const range = document.createRange();
+        range.selectNodeContents(node);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+}
 \$(function(){
-  \$('.${_CSS_PREFIX}-wrapper').click(
+  \$('.${_CSS_PREFIX}-wrapper').on('mouseenter',
     function(e) {
-      \$('#css-class-viewer').text(\"<span class='${_CSS_PREFIX}-wrapper>\" + this.innerHTML.trim() + '</span>');
+      var el = document.getElementById('css-class-viewer');
+      \$(el).text(\"<span class='${_CSS_PREFIX}-wrapper>\" + this.innerHTML.trim() + '</span>');
+      el.focus();
+      selectText(el);
     }
   );
 })
@@ -260,12 +279,15 @@ html="$html
             margin:2px;
             border:1px solid #000;
             background: rgba(255,255,255,0.2);
-            cursor: pointer;
+            /*cursor: pointer;*/
         }
-        .${_CSS_PREFIX}-wrapper:active,
+        .${_CSS_PREFIX}-wrapper:hover {
+          background: rgba(255,255,255,0.5);
+        }
+        /*.${_CSS_PREFIX}-wrapper:active,
         .${_CSS_PREFIX}-wrapper:target {
-          background: rgba(255,255,255,0.65);
-        }
+          background: rgba(255,255,255,0.8);
+        }*/
         .${_CSS_PREFIX}{
            /* background-color:cyan;*/
            background-image: url(img/sprites.png);
